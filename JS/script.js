4df2cc9394caa1fd2445bf6d8cd49416e13ee1cf
Vishal Chavda <123476834+Vishal11799@users.noreply.github.com>
2023-11-19 21:11:14 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,32 @@
+//Compliting task 1 
+
 const studentId = '200520835';
 const studentName = 'Vishal Chavda';
 
 const studentInfoParagraph = document.getElementById('studentInfo');
 studentInfoParagraph.textContent = `Student ID: ${studentId} | Name: ${studentName}`;
 
-// Event listener for the 'Order Now' button
+// function creastion for event to order btn
 document.querySelector('.order_btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the default link behavior
+    event.preventDefault(); 
 
-    // Get values from form inputs
+   // taking values from input 
     const name = document.getElementById('customerName').value;
     const email = document.getElementById('customerEmail').value;
     const number = document.getElementById('customerNumber').value;
     const address = document.getElementById('deliveryAddress').value;
     const pizzaSize = document.getElementById('pizzaSize').value;
-
-    // Get selected crust type
     const crustType = document.querySelector('input[name="crustType"]:checked').value;
-
-    // Get selected pizza ingredients
     const pizzaIngredients = [];
     const selectedIngredients = document.getElementById('pizzaIngredients').selectedOptions;
     for (const option of selectedIngredients) {
         pizzaIngredients.push(option.value);
     }
 
-    // Other values you may want to include in your Pizza object
-    const foodName = "Custom Pizza"; // You can customize this based on user selection
-
-    // Create a Pizza object using the Pizza class
+    // Creating  pizza obeject and class 
     const pizza = new Pizza(name, email, number, foodName, address, pizzaSize, crustType, pizzaIngredients);
 
-    // Display the pizza description on the HTML page
+   // printing the result in end of page
     document.getElementById('orderResult').innerHTML = `
       <h2>Thank you for your order, ${name}!</h2>
       <p>Your pizza details:</p>
@@ -45,7 +40,7 @@ document.querySelector('.order_btn').addEventListener('click', function(event) {
     `;
 });
 
-// Pizza class definition
+// pizza Class 
 class Pizza {
     constructor(name, email, number, foodName, address, pizzaSize, crustType, pizzaIngredients) {
         this.name = name;
